48599fa73551f5459b87b8732b3dec3414b1f832
Eric <dejavuzhou@qq.com>
2017-09-27 16:26:18 +0800

    添加api cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,6 +19,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+        \App\Http\Middleware\PostApiLog::class,
     ];
 
     /**
@@ -40,8 +41,8 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
-            \App\Http\Middleware\PostApiLog::class,
-            \Barryvdh\Cors\HandleCors::class,
+            \Barryvdh\Cors\HandleCors::class,//https://github.com/barryvdh/laravel-cors
+
         ],
     ];
 
