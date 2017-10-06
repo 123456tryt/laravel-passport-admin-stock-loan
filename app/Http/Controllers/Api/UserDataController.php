7fb4e080cd7e98a1e7fb812cc3f74ccd2ac01dbb
chenpz <foo@qq.com>
2017-10-06 09:04:57 +0800

    新增cors

--- app/Http/Controllers/Api/UserDataController.php
+++ app/Http/Controllers/Api/UserDataController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Model\CustBankCard;
-use Illuminate\Http\Response;
 
 class UserDataController extends Controller
 {
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,7 +19,6 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
-        \App\Http\Middleware\PostApiLog::class,
     ];
 
     /**
@@ -41,6 +40,8 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
+            \App\Http\Middleware\PostApiLog::class,
+            \Barryvdh\Cors\HandleCors::class,
         ],
     ];
 
