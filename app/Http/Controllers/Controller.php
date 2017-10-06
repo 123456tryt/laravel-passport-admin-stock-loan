cb45afa0759c4e8a1e639e98a622b84784ac7b07
Eric <dejavuzhou@qq.com>
2017-10-06 10:43:54 +0800

    添加获取验证码的postman文档

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,6 +8,8 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Gregwar\Captcha\CaptchaBuilder;
 use Illuminate\Support\Facades\Cache;
+use phpDocumentor\Reflection\Types\Self_;
+use Zend\Diactoros\Request;
 
 class Controller extends BaseController
 {
@@ -15,6 +17,9 @@ class Controller extends BaseController
 
     const CAPTCHA_PREFIX = "captcha_";
     const CAPTCHA_CACHE = "redis";
+    const CODE_SUCCESS = 1;
+    const Code_FAIL = 0;
+
 
     /**
      * 获取验证码 重新获取验证码
@@ -42,5 +47,23 @@ class Controller extends BaseController
         return $builder->inline();
     }
 
+    /**
+     * @param array $data 返回json 数据体
+     * @param int $code_status 返回 状态
+     * @param string $message 消息
+     * @param \Illuminate\Http\Request|null $request 请求 用于debug
+     * @return \Illuminate\Http\JsonResponse  json返回
+     */
+    static function jsonReturn(array $data = [], int $code_status = self::CODE_SUCCESS, string $message = '', \Illuminate\Http\Request $request = null)
+    {
+        $json['status'] = $code_status;
+        $json['data'] = $data;
+        $json['msg'] = $message;
+        $is_debug = config('app.debug');
+        if ($request && $is_debug) {
+            $json['debug'] = $request ? ($request->all()) : [];
+        }
+        return response()->json($json);
+    }
 
 }
