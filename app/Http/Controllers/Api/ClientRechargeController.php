e19d9d2626521acbe8c36926560a0c92776be7e8
Eric <dejavuzhou@qq.com>
2017-10-25 09:44:31 +0800

    优化搜索功能

--- app/Http/Controllers/Api/ClientRechargeController.php
+++ app/Http/Controllers/Api/ClientRechargeController.php
@@ -3,19 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Model\Agent;
-use App\Http\Model\AgentProfitRateConfig;
 use App\Http\Model\Client;
-use App\Http\Model\ClientAgentEmployeeRelation;
-use App\Http\Model\ClientFeeRate;
 use App\Http\Model\ClientFLow;
 use App\Http\Model\ClientRecharge;
-use App\Http\Model\Employee;
-use App\Http\Model\EmployeeProfitRateConfig;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 
 /**
  * Class ClientRechargeController 客户充值记录
@@ -37,21 +30,27 @@ class ClientRechargeController extends Controller
     {
         $keyword = $request->keyword;
         $status = $request->status;
-
+        $size = $request->input('size', self::PAGE_SIZE);
         $transfer_type = $request->transfer_type;
 
         $query = ClientRecharge::orderByDesc('updated_time');
 
         $range = $request->range;
-        if (count($range) === 2) {
+        if (count($range) == 2 && strlen($range[0]) > 10 && strlen($range[1]) > 10) {
             $from_time = Carbon::parse($range[0]);
             $to_time = Carbon::parse($range[1]);
             $query->whereBetween('created_time', [$from_time, $to_time]);
+            $_GET['page'] = 1;
         }
         if ($keyword) {
-            $query->with(['client' => function ($subQuery) use ($keyword) {
-                $subQuery->orWhere('nick_name', $keyword)->orWhere('real_name', $keyword)->orWhere('cellphone', $keyword);
-            }]);
+            $_GET['page'] = 1;
+            $likeString = "%$keyword%";
+            $client_ids = Client::orWhere('nick_name', 'like', $likeString)
+                ->orWhere('real_name', 'like', $likeString)
+                ->orWhere('cellphone', 'like', $likeString)
+                ->pluck('id')->all();
+            $query = $query->whereIn('cust_id', $client_ids);
+
         } else {
             $query->with('client');
         }
@@ -62,7 +61,7 @@ class ClientRechargeController extends Controller
             $query->where(compact('transfer_type'));
         }
 
-        $list = $query->paginate(self::PAGE_SIZE);
+        $list = $query->paginate($size);
         return self::jsonReturn($list);
     }
 
