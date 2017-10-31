d67310b5a77b30af3b8c0da10cc8da076f63bbf9
Eric <dejavuzhou@qq.com>
2017-10-31 09:23:36 +0800

    客户资金流水搜索关键字调整

--- app/Http/Controllers/Api/ClientFlowController.php
+++ app/Http/Controllers/Api/ClientFlowController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Model\Client;
 use App\Http\Model\ClientFLow;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -29,29 +28,17 @@ class ClientFlowController extends Controller
     {
         $keyword = $request->keyword;
         $page_size = $request->input('size', self::PAGE_SIZE);
-        $flow_type = $request->flow_type;
-        $query = ClientFLow::orderByDesc('created_time');
+        $query = ClientFLow::orderByDesc('created_time')->with('client');
 
-        $range = $request->range;
-        if (count($range) == 2 && strlen($range[0]) > 8 && strlen($range[1]) > 8) {
-            $from_time = Carbon::parse($range[0]);
-            $to_time = Carbon::parse($range[1]);
-            $query->whereBetween('created_time', [$from_time, $to_time]);
-        }
-
-        if ($flow_type) {
-            $query->where(compact('flow_type'));
-        }
         if ($keyword) {
             $likeString = "%$keyword%";
-            $client_ids = Client::orWhere('nick_name', 'like', $likeString)
+            $client_ids = Client::orWhere('id', 'like', $likeString)
                 ->orWhere('real_name', 'like', $likeString)
                 ->orWhere('cellphone', 'like', $likeString)
                 ->pluck('id')->all();
             $query->whereIn('cust_id', array_values($client_ids));
-            $_GET['page'] = 1;
         }
-        $list = $query->with('client')->paginate($page_size);
+        $list = $query->paginate($page_size);
         return self::jsonReturn($list);
     }
 
